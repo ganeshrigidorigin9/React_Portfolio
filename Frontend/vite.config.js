001e999d62c742e0fd0aacfc1ada73a7878049ff
ganeshrigidorigin9 <ganeshganssh8@gmail.com>
2024-12-30 16:32:12 +0530

    Hhb

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,9 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist', // Specify the output directory for the build
+    outDir: 'dist', // Output directory for the built files
   },
-  base: './', // If deploying to the root directory, leave this as './'
-  // If deploying to a subdirectory, change this to: base: '/your-repository-name/'
+  base: './', // Relative paths for assets
 });
-
